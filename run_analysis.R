e4ba63070992e1aaaa47e06c4c29934568332c6a
Ahmed Elborolosy <aelborol@Excellus.com>
2018-08-24 15:59:26 -0400

    Initial filtering of data complete, need to rename columns and relabel activities (1 to 6 -> walking/running)

--- run_analysis.R
+++ run_analysis.R
@@ -1,21 +1,45 @@
 # Coursera Getting and Cleaning Data Final Project
+library(dplyr)
 
 # Merge the training and test sets to create one data set
+
+# Load the files, renaming the columns for the subject & y data to avoid naming issues
 subject.training.data <- read.table('./UCI HAR Dataset/train/subject_train.txt')
-x.training.data <- read.table('./UCI HAR Dataset/train/X_train.txt')
+names(subject.training.data) <- "Activity"
+
 y.training.data <- read.table('./UCI HAR Dataset/train/y_train.txt')
+names(y.training.data) <- "Result"
 
 subject.testing.data <- read.table('./UCI HAR Dataset/test/subject_test.txt')
-x.testing.data <- read.table('./UCI HAR Dataset/test/X_test.txt')
+names(subject.testing.data) <- "Activity"
+
 y.testing.data <- read.table('./UCI HAR Dataset/test/y_test.txt')
+names(y.testing.data) <- "Result"
 
-training.data <- cbind(subject.training.data, x.training.data, y.training.data)
-testing.data <- cbind(subject.testing.data, x.testing.data, y.testing.data)
-
-data.set <- rbind(training.data, testing.data)
+x.training.data <- read.table('./UCI HAR Dataset/train/X_train.txt')
+x.testing.data <- read.table('./UCI HAR Dataset/test/X_test.txt')
 
 activites <- read.table('./UCI HAR Dataset/activity_labels.txt')
 features <- read.table('./UCI HAR Dataset/features.txt')
 
-mean.std.features <- grep("mean|std", features$V2)
-desired.data.set <- data.set[,mean.std.features]
+# Combine the training data tables by column, same w/ testing data
+# Combine the training and testing data together by rows
+training.data <- cbind(subject.training.data, x.training.data, y.training.data)
+testing.data <- cbind(subject.testing.data, x.testing.data, y.testing.data)
+unfiltered.data.set <- rbind(training.data, testing.data)
+
+# Generate list of columns to keep
+mean.std.features <- grep("mean|std", features$V2, ignore.case = TRUE)
+mean.std.features <- sapply(mean.std.features,
+                            function (x) {paste0("V", as.character(x))})
+
+# Replace the V-names with their matching activity label
+
+
+# Add the Activity and Result column names
+mean.std.features <- append(mean.std.features, "Activity", 0)
+mean.std.features <- append(mean.std.features, "Result")
+
+# Filter data set accordingly and then rename it
+filtered.data.set <- unfiltered.data.set[, mean.std.features]
+names(filtered.data.set) <- mean.std.features
